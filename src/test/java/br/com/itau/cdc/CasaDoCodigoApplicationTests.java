e16c3765689fcf19a5f2777d24f533eb914bd3c4
tabita <tabita.barbosa8@gmail.com>
2021-04-05 15:55:37 -0300

    primeiro commit

---
+++ src/test/java/br/com/itau/cdc/CasaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itau.cdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
